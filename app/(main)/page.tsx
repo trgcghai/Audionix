75da87fda1cd55fe70431a4545531b1181a41c78
trgcghai <conghai.tpma@gmail.com>
2025-08-04 15:09:44 +0700

    refactor: remove unused profile query and console log from Home component; add useGetPlaylist hook

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,13 +1,8 @@
 "use client";
-import { useProfileQuery } from "@/services/auth/authApi";
-import MediaList from "../../components/common/MediaList";
-import { mockData } from "../sampleData";
+import MediaList from "@/components/common/MediaList";
+import { mockData } from "@/app/sampleData";
 
 export default function Home() {
-  const { data } = useProfileQuery();
-
-  console.log("Profile Data:", data);
-
   return (
     <div>
       {mockData.map((item, index) => {
